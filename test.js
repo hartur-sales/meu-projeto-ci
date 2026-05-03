37107c767f8b819a3c04f27f3a78a615c932bd52
Hartur <162608728+hartur-sales@users.noreply.github.com>
2026-05-02 23:26:44 -0300

    Add test for hello function

---
+++ test.js
@@ -0,0 +1,9 @@
+const saudacao = require('./hello');
+const resultado = saudacao("Mundo");
+const esperado = "Hello, Mundo!"; 
+if (resultado !== esperado) {
+console.error(` FALHA: esperado "${esperado}", mas
+recebeu "${resultado}"`);
+process.exit(1); 
+}
+console.log(" Todos os testes passaram!");
