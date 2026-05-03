bfd7b013bb4bcc41e5010d02a799342d3600b310
Hartur <162608728+hartur-sales@users.noreply.github.com>
2026-05-02 23:31:10 -0300

    Fix formatting

--- test.js
+++ test.js
@@ -1,9 +1,13 @@
 const saudacao = require('./hello');
+
 const resultado = saudacao("Mundo");
-const esperado = "Hello, Mundo!"; 
+const esperado = "Hello, Mundo!";
+
 if (resultado !== esperado) {
-console.error(` FALHA: esperado "${esperado}", mas
-recebeu "${resultado}"`);
-process.exit(1); 
+  console.error(
+    `FALHA: esperado "${esperado}", mas recebeu "${resultado}"`
+  );
+  process.exit(1);
 }
-console.log(" Todos os testes passaram!");
+
+console.log("Todos os testes passaram!");
